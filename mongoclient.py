b0f48988667e31177295936c046c110294035376
StevonShaw <50302094+StevonShaw@users.noreply.github.com>
2021-06-30 17:22:44 -0700

    update db to be localhost

--- mongoclient.py
+++ mongoclient.py
@@ -1,20 +1,13 @@
 import pymongo
 import json
 
-config = json.load(open('config.json'))
-SERVER_NAME = config['SERVER_NAME']
-USER = config['USER']
-PASSWORD = config['PASSWORD']
+config = json.load(open("config.json"))
+SERVER_NAME = config["SERVER_NAME"]
 
 
 class mongo:
     def __init__(self):
         # Set up database connection
-        self.client = pymongo.MongoClient(
-            SERVER_NAME,
-            username=USER,
-            password=PASSWORD,
-            authSource='collection_name'
-        )
+        self.client = pymongo.MongoClient(SERVER_NAME)
         self.db_collection = self.client.collection_name
         self.batch_limit = 1000 # Mongo caps batch size at 1000
