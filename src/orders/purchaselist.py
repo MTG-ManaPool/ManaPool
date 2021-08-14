18eebb7123358b1a620f010b5eba626495d55453
t-john93 <tjohnson1293@gmail.com>
2021-08-13 17:46:39 -0700

    Added functionality to submit orders to tcg mass entry via get request. Current implementation to use post request (prefered per documentation) in progress.

--- src/orders/purchaselist.py
+++ src/orders/purchaselist.py
@@ -1,14 +1,38 @@
 from cardlist.cardlist import CardList
 import pandas as pd
+import webbrowser
 import requests
 
 class PurchaseList(CardList):
 
     def __init__(self, cardlist):
-        self.cardlist = cardlist.copy(deep=True)
+        self.cardlist = cardlist.cardlist.copy(deep=True)
 
     def submitOrder(self):
+
+        #GET
         print('Submitting your order...')
         baseURL = "https://www.tcgplayer.com/massentry?productline=Magic&utm_campaign=affiliate&utm_medium=AFFILIATECODE&utm_source=AFFILIATECODE"
-        encoding = "&c=1%20Nissa,%20Steward%20of%20Elements||4%20Chart%20a%20Course||"
+        encoding = "&c="
+        for index, card in self.cardlist.iterrows():
+            encoding += f"1{card['name'].replace(' ', '%20')}%20[{card['set'].replace(' ', '%20').upper()}]%7C%7C"
+        
+        url = baseURL + encoding
+        webbrowser.open_new_tab(url)
+
+        #POST
+        # https://api.tcgplayer.com/massentry?partner=AFFILIATECODE&utm_campaign=affiliate&utm_medium=AFFILIATECODE&utm_source=AFFILIATECODE
+
+        # print('Submitting your order...')
+        # postURL = 'https://api.tcgplayer.com/massentry?partner=AFFILIATECODE&utm_campaign=affiliate&utm_medium=AFFILIATECODE&utm_source=AFFILIATECODE'
+        # encoding = "&c="
+        # for index, card in self.cardlist.iterrows():
+        #     encoding += f"1{card['name'].replace(' ', '%20')}%20[{card['set'].replace(' ', '%20').upper()}]%7C%7C"
+
+        # body = {'body': encoding} 
+        # res = requests.post(postURL, data=body)
+        # print(res.headers['Location'])
+        # redirect = res.history
+        # webbrowser.open_new_tab(redirect)
+
         placeholder = input('Press any key to continue...\n')
